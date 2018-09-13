7ece812ab83b2d6c52babfbc1d5deeacafbf547a
Jelte Fennema <github-tech@jeltef.nl>
2018-09-13 10:48:36 +0200

    Don't depend on prop-types

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
   render() {
     const {
       text
