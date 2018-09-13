cc262dc7f1755d79b0af48b0777ee97651dea6cb
Jelte Fennema <github-tech@jeltef.nl>
2018-09-13 12:41:54 +0200

    Lint fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 import styles from './styles.css';
 
-export default class ExampleComponent extends Component<{text: string}> {
+export default class ExampleComponent extends Component<{ text: string }> {
   render() {
     const { text } = this.props;
 
