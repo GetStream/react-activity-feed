397ffc72ca1f8af66905b9cf8253bdec10a9330a
Jaap <jaap@getstream.io>
2018-09-17 16:54:04 +0200

    added IconBadge

--- src/components/IconBadge.js
+++ src/components/IconBadge.js
@@ -1,14 +1,33 @@
-import React from 'react';
+// @flow
+import * as React from 'react';
 import '../App.css';
 import '../styles/IconBadge.css';
+
+export type Props = {|
+  children: React.Node,
+  showNumber?: boolean,
+  hidden?: boolean,
+  feedGroup?: string,
+  userId?: string,
+  unseen: number,
+|};
 
 /**
  * Component is described here.
  *
  * @example ./examples/IconBadge.md
  */
-export default class IconBadge extends React.Component {
+export default class IconBadge extends React.Component<Props> {
   render() {
-    return <React.Fragment>{this.props.children}</React.Fragment>;
+    return (
+      <div className="raf-icon-badge">
+        {this.props.children}
+        {!this.props.hidden && this.props.unseen > 0 ? (
+          <div className="raf-icon-badge__badge">
+            {this.props.showNumber ? <p>{this.props.unseen}</p> : null}
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
