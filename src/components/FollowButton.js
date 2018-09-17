6a6ab9fdaf30d12af9bd42558e2347f756cb677c
Jaap <jaap@getstream.io>
2018-09-17 14:05:13 +0200

    added FollowButton

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -1,14 +1,42 @@
+// @flow
 import React from 'react';
 import '../App.css';
 import '../styles/FollowButton.css';
+
+export type Props = {|
+  /** callback function used on click */
+  clicked?: () => mixed,
+  /** initial follow state */
+  followed?: boolean,
+|};
+
+export type State = {
+  followed: boolean,
+};
 
 /**
  * Component is described here.
  *
  * @example ./examples/FollowButton.md
  */
-export default class FollowButton extends React.Component {
+export default class FollowButton extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { followed: this.props.followed || false };
+  }
+
   render() {
-    return <React.Fragment>{this.props.children}</React.Fragment>;
+    let { clicked, followed } = this.props;
+    return (
+      <div
+        className={`raf-follow-button ${
+          followed ? 'raf-follow-button--active' : ''
+        }`}
+        role="button"
+        onClick={clicked}
+      >
+        {followed ? 'Following' : 'Follow'}
+      </div>
+    );
   }
 }
