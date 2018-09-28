59b0da507f3616e2862d700d144bf979f3346f42
Jaap <jaap@getstream.io>
2018-09-28 14:46:26 +0200

    added variables to styles

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
+
 import atImport from 'postcss-easy-import';
 import cssnext from 'postcss-cssnext';
 import nested from 'postcss-nested';
@@ -13,10 +14,36 @@ import vars from 'postcss-simple-vars';
 
 import pkg from './package.json';
 
-const colors = {
+const variables = {
+  borderRadius: '4px',
+
+  z0: '0',
+  z1: '10',
+  z2: '20',
+  z3: '30',
+  z4: '40',
+  z5: '50',
+  z6: '60',
+  z7: '70',
+  z8: '80',
+  z9: '90',
+
+  transparent: 'rgba(255,255,255,0)',
+  white: '#ffffff',
+  black: '#000000',
   primary: '#00D46A',
   info: '#0BA8E0',
   faded: '#DDDDDD',
+  error: '#FF0000',
+  fontColor: '#414D54',
+
+  // Colors to optimize
+  rafAttachedActivityAuthor: '#414D54',
+  rafActivityFooterBorder: '#E6F0F2',
+  rafCommentfieldBackground: '#F7F7F7', // lightest grey
+  rafCardBackground: '#F4F4F4', // lighter grey
+  rafCardBorder: '#A0B2B8', // grey
+  rafDropdownBackground: '#313E47', // dark grey
 };
 
 export default {
@@ -39,7 +66,7 @@ export default {
       plugins: [
         atImport(),
         nested(),
-        vars({ variables: colors }),
+        vars({ variables }),
         colorFunction(),
         cssnext(),
       ],
