9960f5826f9f4ad5fdd5bb8b2ab469c85b1e52b9
Jaap <jaap@getstream.io>
2018-10-03 17:02:25 +0200

    added retry button to imagepreviewer

--- src/components/ImagePreviewer.js
+++ src/components/ImagePreviewer.js
@@ -37,9 +37,14 @@ export default class ImagePreviewer extends React.Component<Props> {
             <div key={image.id} className="raf-image-previewer__image">
               {image.state === 'uploading' && <LoadingIndicator />}
               {image.state === 'failed' && (
-                <div onClick={handleRetry && (() => handleRetry(image.id))}>
-                  Retry?
-                </div>
+                <div
+                  className="raf-image-previewer__retry"
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      '<svg width="22" height="20" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path d="M20 5.535V2a1 1 0 0 1 2 0v6a1 1 0 0 1-1 1h-6a1 1 0 0 1 0-2h3.638l-2.975-2.653a8 8 0 1 0 1.884 8.32 1 1 0 1 1 1.886.666A10 10 0 1 1 5.175 1.245c3.901-2.15 8.754-1.462 11.88 1.667L20 5.535z" fill="#FFF" fill-rule="nonzero"/></svg>',
+                  }}
+                  onClick={handleRetry && (() => handleRetry(image.id))}
+                />
               )}
               <Thumbnail
                 handleClose={handleRemove && this._handleClose}
