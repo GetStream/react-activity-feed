87285308da44c1059e366fa11acdb59ebf8b50e0
Jaap <jaap@getstream.io>
2018-10-04 17:48:18 +0200

    started render function of b2b timeline

--- src/components/B2BTimeline.js
+++ src/components/B2BTimeline.js
@@ -1,6 +1,21 @@
 import * as React from 'react';
-// import TimeHeader from './TimeHeader';
+import TimeHeader from './TimeHeader';
 // import B2BActivity from './B2BActivity';
+
+const MONTHS = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December',
+};
 
 /**
  * Component is described here.
@@ -8,7 +23,7 @@ import * as React from 'react';
  * @example ./examples/B2BTimeline.md
  */
 export default class B2BTimeline extends React.Component {
-  sortByYearMonth = (activities) => {
+  sortByYearMonth(activities) {
     const newActivities = {};
     activities.map((activity) => {
       const month = new Date(activity.timestamp).getMonth();
@@ -16,22 +31,46 @@ export default class B2BTimeline extends React.Component {
       if (newActivities[year] === undefined) {
         newActivities[year] = {};
         newActivities[year][month] = [];
-        newActivities[year][month].push(activity);
+        return newActivities[year][month].push(activity);
       } else if (newActivities[year][month] === undefined) {
         newActivities[year][month] = [];
-        newActivities[year][month].push(activity);
+        return newActivities[year][month].push(activity);
       } else {
-        newActivities[year][month].push(activity);
+        return newActivities[year][month].push(activity);
       }
     });
 
     return newActivities;
-  };
+  }
+
+  timeline(activities) {
+    const sorted = this.sortByYearMonth(activities);
+    return Object.keys(sorted)
+      .sort((a, b) => b - a)
+      .map(function(year) {
+        return Object.keys(sorted[year])
+          .sort((a, b) => b - a)
+          .map(function(month, i) {
+            return (
+              <React.Fragment key={`month-${month}-${i}`}>
+                <TimeHeader>{`${MONTHS[month]} ${year}`}</TimeHeader>
+                {sorted[year][month].map((
+                  post,
+                  i, // map over posts in month
+                ) => (
+                  <p key={`post-${i}`}>
+                    <code>{JSON.stringify(post)}</code>
+                  </p>
+                ))}
+              </React.Fragment>
+            );
+          });
+      });
+  }
 
   render() {
     const icon =
       '<svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg"><path d="M19.99 7c0-.72-.37-1.35-.94-1.7L10 0 .95 5.3C.38 5.65 0 6.28 0 7v10c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2l-.01-10zM10 12L1.74 6.84 10 2l8.26 4.84L10 12z" fill="#A0B2B8" fill-rule="evenodd"/></svg>';
-
     return (
       <div className="raf-b2btimeline">
         <div className="raf-b2btimeline__line">
@@ -47,15 +86,7 @@ export default class B2BTimeline extends React.Component {
           />
         </div>
         <div className="raf-b2btimeline__feed">
-          {Object.keys(this.sortByYearMonth(this.props.activities)).forEach(
-            (key) => (
-              <p>{key}</p>
-            ),
-          )}
-
-          {/* {this.props.activities.map((activity ,i) => (
-            <B2BActivity activity={activity} />
-          ))} */}
+          {this.timeline(this.props.activities)}
         </div>
       </div>
     );
--- src/components/examples/B2BTimeline.md
+++ src/components/examples/B2BTimeline.md
@@ -30,6 +30,54 @@ const activities = [
     },
     own_reactions: {},
     reaction_counts: {},
+    timestamp: new Date() - 1000000000,
+  },
+  {
+    object: {
+      verb: 'tracked email',
+      actor: {
+        name: 'Cindy Smith',
+      },
+      subject: 'Hello',
+      content: 'Lispum',
+      status: 'sent',
+      clicks: '12',
+      opens: '1',
+    },
+    own_reactions: {},
+    reaction_counts: {},
+    timestamp: new Date() - 3000000000,
+  },
+  {
+    object: {
+      verb: 'tracked email',
+      actor: {
+        name: 'Cindy Smith',
+      },
+      subject: 'Hello',
+      content: 'Lispum',
+      status: 'sent',
+      clicks: '12',
+      opens: '1',
+    },
+    own_reactions: {},
+    reaction_counts: {},
+    timestamp: new Date() - 8000000000,
+  },
+  {
+    object: {
+      verb: 'tracked email',
+      actor: {
+        name: 'Cindy Smith',
+      },
+      subject: 'Hello',
+      content: 'Lispum',
+      status: 'sent',
+      clicks: '12',
+      opens: '1',
+    },
+    own_reactions: {},
+    reaction_counts: {},
     timestamp: new Date() - 113000000000,
   },
 ];
