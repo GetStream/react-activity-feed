ac64f3e299dc6e0d2fdcf684c19a438026aac8ca
Jelte Fennema <github-tech@jeltef.nl>
2018-11-05 17:35:44 +0100

    CommentItem: Add onClickUser

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -11,6 +11,7 @@ import { humanizeTimestamp } from '../utils';
 
 export type Props = {|
   comment: Comment,
+  onClickUser?: (?any) => mixed,
 |};
 
 /**
@@ -19,12 +20,29 @@ export type Props = {|
  * @example ./examples/CommentItem.md
  */
 export default class CommentItem extends React.Component<Props> {
+  _user = () => {
+    const { user } = this.props.comment;
+    return user;
+  };
+
+  onClickUser = () => {
+    const { onClickUser } = this.props;
+    if (onClickUser) {
+      return onClickUser(this._user());
+    }
+  };
+
+  _getOnClickUser() {
+    return this.props.onClickUser ? this.onClickUser : undefined;
+  }
+
   render() {
     const { comment } = this.props;
     return (
       <div className="raf-comment-item">
         <Flex a="flex-start" style={{ padding: '8px 0' }}>
           <Avatar
+            onClick={this._getOnClickUser()}
             image={comment.user.data.profileImage || placeholder}
             circle
             size={25}
@@ -36,7 +54,10 @@ export default class CommentItem extends React.Component<Props> {
               <small>{humanizeTimestamp(comment.created_at)}</small>
             </time>
             <p>
-              <span className="raf-comment-item__author">
+              <span
+                onClick={this._getOnClickUser()}
+                className="raf-comment-item__author"
+              >
                 {comment.user.data.name}
               </span>{' '}
               {comment.data.text}
