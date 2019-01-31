37517a9f91cbb1d26791afb1210625c519342791
Jelte Fennema <github-tech@jeltef.nl>
2019-01-31 16:12:17 +0100

    Security fix: Fix XSS issue for unsanitized urls

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import placeholder from '../images/placeholder.png';
 import IconButton from './IconButton';
+import { sanitizeURL } from '../utils';
 
 export type Props = {|
   image?: ?string,
@@ -43,11 +44,12 @@ export default class Card extends React.Component<Props> {
     if (!image && images && images.length) {
       image = images[0].image;
     }
+    const url = sanitizeURL(this.props.url);
     const svg =
       '<svg width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M465 5c5.53 0 10 4.47 10 10s-4.47 10-10 10-10-4.47-10-10 4.47-10 10-10zm3.59 5L465 13.59 461.41 10 460 11.41l3.59 3.59-3.59 3.59 1.41 1.41 3.59-3.59 3.59 3.59 1.41-1.41-3.59-3.59 3.59-3.59-1.41-1.41z" id="b"/><filter x="-30%" y="-30%" width="160%" height="160%" filterUnits="objectBoundingBox" id="a"><feOffset in="SourceAlpha" result="shadowOffsetOuter1"/><feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0" in="shadowBlurOuter1"/></filter></defs><g transform="translate(-451 -1)" fill-rule="nonzero" fill="none"><use fill="#000" filter="url(#a)" xlink:href="#b"/><use fill="#FFF" fill-rule="evenodd" xlink:href="#b"/></g></svg>';
     return (
       <a
-        href={!nolink ? this.props.url : null}
+        href={!nolink ? url : null}
         target="blank"
         rel="noopener"
         className={`raf-card ${
@@ -70,9 +72,7 @@ export default class Card extends React.Component<Props> {
         <div className="raf-card__content">
           <div className="raf-card__content-left">
             <p className="raf-card__title">{this.props.title}</p>
-            <p className="raf-card__url">
-              {this.trimUrl(this.props.url || '')}
-            </p>
+            <p className="raf-card__url">{this.trimUrl(url || '')}</p>
             <p className="raf-card__description">{this.props.description}</p>
           </div>
           {handleClose && image === undefined ? (
--- src/components/FilePreviewer.js
+++ src/components/FilePreviewer.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import FileIcon from './FileIcon';
 import LoadingIndicator from './LoadingIndicator';
+import { sanitizeURL } from '../utils';
 import type { FileUpload } from '../types';
 
 type Props = {|
@@ -38,7 +39,7 @@ export default class FilePreviewer extends React.Component<Props> {
               >
                 <FileIcon mimeType={upload.file.type} />
 
-                <a href={upload.url} download>
+                <a href={sanitizeURL(upload.url)} download>
                   {upload.file.name}
                   {upload.state === 'failed' && (
                     <React.Fragment>
