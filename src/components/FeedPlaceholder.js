72c80232e97687e0411ae4046f90cffafcd6c74c
loginloop <trungnghia260114@gmail.com>
2019-11-27 13:56:25 +0700

    Render no data

--- src/components/FeedPlaceholder.js
+++ src/components/FeedPlaceholder.js
@@ -6,9 +6,6 @@ type Props = {|
   text: string,
 |};
 export default class FeedPlaceholder extends React.Component<Props> {
-  static defaultProps = {
-    text: 'No data to display...',
-  };
 
   render() {
     return (
