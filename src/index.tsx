1133dfece824920ed6f641fe44b443c7b54c5f87
Anton Arnautov <arnautov.anton@gmail.com>
2021-03-05 02:17:38 +0100

    Fixed imports and exports

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ export { default as LoadMorePaginator } from './components/LoadMorePaginator';
 
 // Activity sub components
 export { default as ActivityFooter } from './components/ActivityFooter';
-export { default as AttachedActivity } from './components/AttachedActivity';
+export * from './components/AttachedActivity';
 export { default as UserBar } from './components/UserBar';
 export { default as Card } from './components/Card';
 export { default as Gallery } from './components/Gallery';
