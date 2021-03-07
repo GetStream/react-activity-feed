4d5f4448f370a24faac6928526984cc00c16efbd
Anton Arnautov <arnautov.anton@gmail.com>
2021-03-07 15:43:10 +0100

    Fixed change requests

--- src/components/AttachedActivity.tsx
+++ src/components/AttachedActivity.tsx
@@ -1,30 +1,29 @@
 import React, { useMemo } from 'react';
-import { userOrDefault } from '../utils';
-
 import { EnrichedActivity, OGAPIResponse } from 'getstream';
-
 import { Thumbnail } from 'react-file-utils';
 
-export type FileData = {
+import { userOrDefault } from '../utils';
+
+type FileData = {
   mimeType: string;
   name: string;
   url: string;
 };
 
-export type Attachments = {
+type Attachments = {
   files?: FileData[];
   images?: string[];
   og?: OGAPIResponse;
 };
 
-export type CustomActivityData = {
+type CustomActivityData = {
   attachments?: Attachments;
   image?: string;
   link?: boolean;
   text?: string;
 };
 
-export type UserData = {
+type UserData = {
   name?: string;
   profileImage?: string;
 };
@@ -37,26 +36,24 @@ export const AttachedActivity = ({ activity: { object, verb, attachments, actor 
   const images = attachments?.images ?? [];
   const user = useMemo(() => userOrDefault(actor), [actor]);
 
-  if (verb === 'repost' || verb === 'post' || verb === 'comment') {
-    return (
-      <div className="raf-attached-activity">
-        {images.length ? (
-          <div className="raf-attached-activity__images">
-            {images.slice(0, 5).map((image, i) => (
-              <Thumbnail image={image} size={50} key={`image-${i}`} />
-            ))}
-          </div>
-        ) : (
-          <React.Fragment>
-            <p className="raf-attached-activity__author">
-              <strong>{user.data.name}</strong>
-            </p>
-            <p className="raf-attached-activity__content">{object as string}</p>
-          </React.Fragment>
-        )}
-      </div>
-    );
-  }
+  if (verb !== 'repost' && verb !== 'post' && verb !== 'comment') return null;
 
-  return null;
+  return (
+    <div className="raf-attached-activity">
+      {images.length ? (
+        <div className="raf-attached-activity__images">
+          {images.slice(0, 5).map((image, i) => (
+            <Thumbnail image={image} size={50} key={`image-${i}`} />
+          ))}
+        </div>
+      ) : (
+        <React.Fragment>
+          <p className="raf-attached-activity__author">
+            <strong>{user.data.name}</strong>
+          </p>
+          <p className="raf-attached-activity__content">{object as string}</p>
+        </React.Fragment>
+      )}
+    </div>
+  );
 };
