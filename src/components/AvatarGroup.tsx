3e043a03af765070655e0a30654c326862d9868a
Anton Arnautov <arnautov.anton@gmail.com>
2021-03-16 23:15:46 +0100

    Fixed typing, removed unnecessary comments

--- src/components/AvatarGroup.tsx
+++ src/components/AvatarGroup.tsx
@@ -1,60 +1,29 @@
 import React, { useMemo, MouseEvent } from 'react';
-import { UserAPIResponse } from 'getstream';
-import { Avatar } from './Avatar';
+import { EnrichedUser } from 'getstream';
+
 import { userOrDefault } from '../utils';
+import { DefaultUT } from '../Context/StreamApp';
+import { Avatar } from './Avatar';
 
-type UserData = {
-  name?: string;
-  profileImage?: string;
-};
-
-// export type Props = {|
-// 	/* The list of users to display */
-// 	users: UserResponse[],
-// 	/* The size of the avatar in px */
-// 	avatarSize: number,
-// 	/* The limit of avatars to display */
-// 	limit: number,
-// 	/* Callback to call when clicking on a user in the notification */
-// 	onClickUser?: (UserResponse) => mixed,
-//  |};
-
-export type AvatarGroupProps<UserType extends UserData> = {
+export type AvatarGroupProps<UT extends DefaultUT = DefaultUT> = {
   avatarSize?: number;
   limit?: number;
-  onClickUser?: (user: UserAPIResponse<UserType>) => void;
-  users?: Array<UserAPIResponse<UserType>>;
+  onClickUser?: (user: ReturnType<typeof userOrDefault>) => void;
+  users?: Array<EnrichedUser<UT>>;
 };
 
-// static defaultProps = {
-// 	avatarSize: 30,
-// 	limit: 5,
-//  };
-
-//  _getOnClickUser(user) {
-// 	return this.props.onClickUser ? (e) => this.onClickUser(e, user) : undefined;
-//  }
-
-//  onClickUser = (e, user) => {
-// 	const { onClickUser } = this.props;
-// 	if (onClickUser) {
-// 	  e.stopPropagation();
-// 	  return onClickUser(userOrDefault(user));
-// 	}
-//  };
-
-export function AvatarGroup<UserType extends UserData>({
+export function AvatarGroup<UT extends DefaultUT = DefaultUT>({
   limit = 5,
   users = [],
-  avatarSize = 50,
+  avatarSize = 30,
   onClickUser,
-}: AvatarGroupProps<UserType>) {
+}: AvatarGroupProps<UT>) {
   const handleClick = useMemo(
     () =>
       onClickUser
-        ? (user: UserAPIResponse<UserType>) => (event: MouseEvent<HTMLImageElement | SVGSVGElement>) => {
+        ? (user: EnrichedUser<UT>) => (event: MouseEvent<HTMLImageElement | SVGSVGElement>) => {
             event.stopPropagation();
-            onClickUser(userOrDefault(user));
+            onClickUser(userOrDefault<UT>(user));
           }
         : undefined,
     [onClickUser],
@@ -64,7 +33,7 @@ export function AvatarGroup<UserType extends UserData>({
     <div className="raf-avatar-group">
       {users.slice(0, limit).map((user, i) => (
         <div className="raf-avatar-group__avatar" key={`avatar-${i}`}>
-          <Avatar onClick={handleClick?.(user)} image={user?.data.profileImage} size={avatarSize} circle />
+          <Avatar onClick={handleClick?.(user)} image={user.data?.profileImage} size={avatarSize} circle />
         </div>
       ))}
     </div>
