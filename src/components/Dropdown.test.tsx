2f4543b5f2eb7d5c4cec6b4c7676d40f0d63e97a
Anton Arnautov <arnautov.anton@gmail.com>
2021-03-16 19:22:33 +0100

    Extended test

--- src/components/Dropdown.test.tsx
+++ src/components/Dropdown.test.tsx
@@ -74,5 +74,32 @@ describe('Dropdown', () => {
         </div>
       </div>
     `);
+
+    fireEvent.click(container);
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div
+        class="raf-dropdown"
+      >
+        <div
+          class="rfu-icon-button"
+          role="button"
+        >
+          <svg
+            class="raf-dropdown__button"
+            height="8"
+            viewBox="0 0 12 8"
+            width="12"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.41 0L6 4.77 10.59 0 12 1.469l-6 6.25-6-6.25z"
+              fill="#A0B2B8"
+              fill-rule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+    `);
   });
 });
