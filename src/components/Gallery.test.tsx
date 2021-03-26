fc62362441b41d48e118882189275c3eb7fe2913
Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
2021-03-26 09:09:29 +0100

    FSDK-36: Test Gallery (#197)
    * Added test and temporary export fix
    
    * Updated test snapshots (added roles)

---
+++ src/components/Gallery.test.tsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { Gallery } from './Gallery';
+
+jest.mock(
+  'react-images',
+  // eslint-disable-next-line react/display-name
+  () => ({ isOpen, onClose, currentImage }: { isOpen: boolean; onClose: () => void; currentImage?: number }) => (
+    <>
+      {isOpen && (
+        <div onClick={onClose}>
+          lightbox <span>{currentImage}</span>
+        </div>
+      )}
+    </>
+  ),
+);
+
+const testData = [
+  'https://source.unsplash.com/random/800x600',
+  'https://source.unsplash.com/random/800x601',
+  'https://source.unsplash.com/random/800x603',
+];
+
+describe('Gallery', () => {
+  it('renders with an empty images array', () => {
+    const tree = renderer.create(<Gallery images={[]} />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <div
+        className="raf-gallery"
+      />
+    `);
+  });
+
+  it('renders with one image', () => {
+    const tree = renderer.create(<Gallery images={[testData[0]]} />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <div
+        className="raf-gallery"
+      >
+        <div
+          className="img "
+          onClick={[Function]}
+          role="button"
+        >
+          <img
+            alt=""
+            className="raf-gallery__image"
+            src="https://source.unsplash.com/random/800x600"
+          />
+        </div>
+      </div>
+    `);
+  });
+
+  it('renders with multiple images', () => {
+    const tree = renderer.create(<Gallery images={testData} />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <div
+        className="raf-gallery"
+      >
+        <div
+          className="img "
+          onClick={[Function]}
+          role="button"
+        >
+          <img
+            alt=""
+            className="raf-gallery__image"
+            src="https://source.unsplash.com/random/800x600"
+          />
+        </div>
+        <div
+          className="img "
+          onClick={[Function]}
+          role="button"
+        >
+          <img
+            alt=""
+            className="raf-gallery__image"
+            src="https://source.unsplash.com/random/800x601"
+          />
+        </div>
+        <div
+          className="img "
+          onClick={[Function]}
+          role="button"
+        >
+          <img
+            alt=""
+            className="raf-gallery__image"
+            src="https://source.unsplash.com/random/800x603"
+          />
+        </div>
+      </div>
+    `);
+  });
+
+  it('renders Lightbox after click on one of the images', () => {
+    const { getByAltText, getByText, container } = render(<Gallery images={[testData[0]]} />);
+
+    fireEvent.click(getByAltText(''));
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div
+        class="raf-gallery"
+      >
+        <div
+          class="img "
+          role="button"
+        >
+          <img
+            alt=""
+            class="raf-gallery__image"
+            src="https://source.unsplash.com/random/800x600"
+          />
+        </div>
+        <div>
+          lightbox 
+          <span>
+            0
+          </span>
+        </div>
+      </div>
+    `);
+
+    fireEvent.click(getByText('lightbox'));
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div
+        class="raf-gallery"
+      >
+        <div
+          class="img "
+          role="button"
+        >
+          <img
+            alt=""
+            class="raf-gallery__image"
+            src="https://source.unsplash.com/random/800x600"
+          />
+        </div>
+      </div>
+    `);
+  });
+});
