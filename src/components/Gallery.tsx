4e40580ae0ab22fb61ea6eb5849b42660a402197
Amin Mahboubi <aminmahboobi@gmail.com>
2021-05-04 13:52:50 +0200

    gallery hide arrows on first/last slide

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,30 +7,29 @@ export type GalleryProps = {
 
 export const Gallery = ({ images = [] }: GalleryProps) => {
   const [index, setIndex] = useState<number | null>(null);
-  const len = images.length;
 
   return (
     <div className="raf-gallery">
       {images.slice(0, 5).map((image, i) => (
         <div
           role="button"
-          className={`img ${i === 4 && len > 5 ? 'img--last' : ''}`}
+          className={`img ${i === 4 && images.length > 5 ? 'img--last' : ''}`}
           onClick={() => setIndex(i)}
           key={`image-${i}`}
         >
           <img src={image} className="raf-gallery__image" alt="" />
-          {i === 4 && len > 5 && <p>{len - 4} more</p>}
+          {i === 4 && images.length > 5 && <p>{images.length - 4} more</p>}
         </div>
       ))}
 
       {index !== null && (
         <Lightbox
           mainSrc={images[index]}
-          nextSrc={images[(index + 1) % len]}
-          prevSrc={images[(index + len - 1) % len]}
+          nextSrc={images[index + 1]}
+          prevSrc={images[index - 1]}
           onCloseRequest={() => setIndex(null)}
-          onMoveNextRequest={() => setIndex((index + 1) % len)}
-          onMovePrevRequest={() => setIndex((index + len - 1) % len)}
+          onMoveNextRequest={() => setIndex(index + 1)}
+          onMovePrevRequest={() => setIndex(index - 1)}
         />
       )}
     </div>
