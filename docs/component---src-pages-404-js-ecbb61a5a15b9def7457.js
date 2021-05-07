9fedf93643a08098e3d33c8fdb8053536dd928fd
Amin Mahboubi <aminmahboobi@gmail.com>
2021-05-07 17:06:45 +0200

    1.0.0

---
+++ docs/component---src-pages-404-js-ecbb61a5a15b9def7457.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ecbb61a5a15b9def7457.js.map
