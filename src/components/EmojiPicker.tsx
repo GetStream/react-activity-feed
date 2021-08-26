ecf736dad9f67d5da6293eb17a50912ae227e345
Anton Arnautov <arnautov.anton@gmail.com>
2021-08-27 00:03:33 +0200

    Added i18n property to emoji picker

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -28,7 +28,33 @@ export const EmojiPicker = ({ emojiData = defaultEmojiData, onSelect }: EmojiPic
     <div className="raf-emoji-picker">
       {open && (
         <div data-testid="picker-wrapper" className="raf-emoji-picker__container" ref={emojiPicker}>
-          <NimbleEmojiPicker emoji="point_up" title={t('Pick your emoji')} data={emojiData} onSelect={onSelect} />
+          <NimbleEmojiPicker
+            i18n={{
+              search: t('Search'),
+              clear: t('Clear'),
+              notfound: t('No emoji found'),
+              skintext: t('Choose your default skin tone'),
+              categorieslabel: t('Emoji categories'),
+              categories: {
+                search: t('Search Results'),
+                recent: t('Frequently Used'),
+                smileys: t('Smileys & Emotion'),
+                people: t('People & Body'),
+                nature: t('Animals & Nature'),
+                foods: t('Food & Drink'),
+                activity: t('Activity'),
+                places: t('Travel & Places'),
+                objects: t('Objects'),
+                symbols: t('Symbols'),
+                flags: t('Flags'),
+                custom: t('Custom'),
+              },
+            }}
+            emoji="point_up"
+            title={t('Pick your emoji')}
+            data={emojiData}
+            onSelect={onSelect}
+          />
         </div>
       )}
       <div role="button" onClick={() => setOpen(true)} className="raf-emoji-picker__button">
