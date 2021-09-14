7ffa7ac423de374f7fae115d08c395bc56372848
MartinCupela <martin.cupela@gmail.com>
2021-09-14 10:24:48 +0200

    test custom emoji picker translations

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -29,7 +29,26 @@ function App() {
           <h3>React Activity Feed</h3>
           <NotificationDropdown right />
         </div>
-        <StatusUpdateForm />
+        <StatusUpdateForm
+          emojiI18n={{
+            search: 'Testing it',
+            notfound: 'Not found string',
+            skintext: 'Skin text',
+            categories: {
+              search: 'Categories/search',
+              activity: 'Categories/activity',
+              custom: 'Categories/custom',
+              flags: 'Categories/flags',
+              foods: 'Categories/foods',
+              nature: 'Categories/nature',
+              objects: 'Categories/objects',
+              people: 'Categories/people',
+              places: 'Categories/places',
+              recent: 'Categories/recent',
+              symbols: 'Categories/symbols',
+            },
+          }}
+        />
         <FlatFeed
           notify
           feedGroup="user"
