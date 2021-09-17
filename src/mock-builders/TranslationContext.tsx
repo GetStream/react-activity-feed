73c68a6c48ef740126707a77379d337459b8b89e
MartinCupela <martin.cupela@gmail.com>
2021-09-17 09:19:55 +0200

    fix lint errors

--- src/mock-builders/TranslationContext.tsx
+++ src/mock-builders/TranslationContext.tsx
@@ -6,6 +6,7 @@ export const translationProviderData = { t: String, tDateTimeParser: moment };
 
 type TranslationProviderMockProps = PropsWithChildren<{ value?: TranslationContextValue }>;
 
-export const TranslationProviderMock = ({ value = translationProviderData, children }: TranslationProviderMockProps) => (
-  <TranslationProvider value={value}>{children}</TranslationProvider>
-);
+export const TranslationProviderMock = ({
+  value = translationProviderData,
+  children,
+}: TranslationProviderMockProps) => <TranslationProvider value={value}>{children}</TranslationProvider>;
