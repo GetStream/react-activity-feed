d1dc9d1c23c8906e7ab5b6f517814db09b189837
MartinCupela <32706194+MartinCupela@users.noreply.github.com>
2021-09-17 06:46:59 +0200

    Update src/mock-builders/TranslationContext.tsx
    Use default props
    
    Co-authored-by: Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>

--- src/mock-builders/TranslationContext.tsx
+++ src/mock-builders/TranslationContext.tsx
@@ -6,6 +6,6 @@ export const translationProviderData = { t: String, tDateTimeParser: moment };
 
 type TranslationProviderMockProps = PropsWithChildren<{ value?: TranslationContextValue }>;
 
-export const TranslationProviderMock = ({ value, children }: TranslationProviderMockProps) => (
-  <TranslationProvider value={value || translationProviderData}>{children}</TranslationProvider>
+export const TranslationProviderMock = ({ value = translationProviderData, children }: TranslationProviderMockProps) => (
+  <TranslationProvider value={value}>{children}</TranslationProvider>
 );
