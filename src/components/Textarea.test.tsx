35e0ed340776e175e0e446a1392e10e491afe217
MartinCupela <martin.cupela@gmail.com>
2022-03-08 23:30:03 +0100

    test: Textarea forwards native textarea HTML attributes to the underlying textarea element

--- src/components/Textarea.test.tsx
+++ src/components/Textarea.test.tsx
@@ -24,4 +24,27 @@ describe('Textarea', () => {
       </div>
     `);
   });
+
+  it('forwards native textarea HTML attributes', () => {
+    const tree = renderer.create(<Textarea required={true} role={'dialog'} />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <div
+        className="rta  raf-textarea"
+      >
+        <textarea
+          className="rta__textarea raf-textarea__textarea"
+          onBlur={[Function]}
+          onChange={[Function]}
+          onClick={[Function]}
+          onScroll={[Function]}
+          onSelect={[Function]}
+          placeholder="Share your opinion"
+          required={true}
+          role="dialog"
+          rows={3}
+          value=""
+        />
+      </div>
+    `);
+  });
 });
